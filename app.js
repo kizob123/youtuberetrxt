1ed42db8f30627f165151196a7692302da8044c2
iokaddbiz-dotcom <iokaddbiz@gmail.com>
2026-09-10 13:56:50 +0100

    Fix cloud ip restriction block via frontend offloading 15

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Force JSON parsed body routing
+app.use(express.json());
+
 // Serve the frontend files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
